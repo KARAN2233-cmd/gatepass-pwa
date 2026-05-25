34e818d82b89ce6ee0be7295460222c614381eac
Vishnu <mis.ajmerind@gmail.com>
2026-05-25 14:52:20 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -12,12 +12,12 @@ importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey:            self.FIREBASE_API_KEY,
-  authDomain:        self.FIREBASE_AUTH_DOMAIN,
-  projectId:         self.FIREBASE_PROJECT_ID,
-  storageBucket:     self.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: self.FIREBASE_MESSAGING_SENDER_ID,
-  appId:             self.FIREBASE_APP_ID
+  apiKey            : "AIzaSyDbX44eylIbr2Fa9gRv7W5cJB0rWCvJ0W4",
+  authDomain        : "ajmergatepass.firebaseapp.com",
+  projectId         : "ajmergatepass",
+  storageBucket     : "ajmergatepass.firebasestorage.app",
+  messagingSenderId : "704734310536",
+  appId             : "1:704734310536:web:5e9f46249c062077012f3d"
 });
 
 const messaging = firebase.messaging();
